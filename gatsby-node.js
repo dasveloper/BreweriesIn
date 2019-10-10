f87bc801d3c2c525aa5eb7bd811b5a8c07dd6d14
dasveloper <dasveloper@gmail.com>
2019-10-10 18:12:28 -0400

    Test slice

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,7 +48,7 @@ exports.createPages = async ({ graphql, actions }) => {
     return a.name.localeCompare(b.name);
   });
 
-  breweries.forEach((brewery) => {
+  breweries.slice(0,20).forEach((brewery) => {
     if (brewery.country !== "United States" || brewery.brewery_type === "planning"){
       return;
     }
@@ -66,7 +66,7 @@ exports.createPages = async ({ graphql, actions }) => {
         context: brewery,
     })
   });
-
+   return;
   stateSet.forEach(state => {
     createPage({
       path: `/${kebabCase(state)}/`,
