cdf3664ba277d73503346f1e05a7d1155125af84
dasveloper <dasveloper@gmail.com>
2019-10-11 10:02:39 -0400

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -51,21 +51,26 @@ exports.createPages = async ({ graphql, actions }) => {
     if (brewery.country !== "United States" || brewery.brewery_type === "planning"){
       return;
     }
+    let slug = '';
     if (brewery.state){
       stateSet.add(brewery.state);
+      slug+=`/${kebabCase(brewery.state)}/`;
     }
 
     if (brewery.city){
       citySet.add(brewery.city);
+      slug+=`/${kebabCase(brewery.city)}/`;
+
     }
+    slug+=`/${kebabCase(brewery.name)}/`;
 
     createPage({
-        path: brewery.slug,
+        path: slug,
         component: breweryPage,
         context: brewery,
     })
   });
-
+  
    return;
   stateSet.forEach(state => {
     createPage({
