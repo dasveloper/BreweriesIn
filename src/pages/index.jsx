7e9a7150c56b58a6bf3bca156c08db8623dfcfdd
dasveloper <dasveloper@gmail.com>
2019-10-12 14:06:59 -0400

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,124 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { graphql } from "gatsby";
 import Layout from "../layout";
-import PostListing from "../components/PostListing/PostListing";
-import SEO from "../components/SEO/SEO";
+import CityList from "../components/CityList/CityList";
+import { Link } from "gatsby";
+import dashify from 'dashify';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import BreweryListing from "../components/BreweryListing/BreweryListing";
+
 import config from "../../data/SiteConfig";
 
-class Index extends React.Component {
+export default class index extends React.Component {
   render() {
-   // const postEdges = this.props.data.allMarkdownRemark.edges;
+    console.log(this.props)
+    const breweries = this.props.data.allSitePage.edges;
+    // const { tag } = this.props.pageContext;
+    // const postEdges = this.props.data.allMarkdownRemark.edges;
+    const citySet = new Set();
+
+    breweries.forEach((brewery) => {
+      const { postal_code, city } = brewery.node.context;
+
+      if (city) {
+        citySet.add(city);
+      }
+    });
     return (
       <Layout>
-        <div className="index-container">
-          <Helmet title={config.siteTitle} />
-          {/* <SEO />
-          <PostListing postEdges={postEdges} /> */}
-        </div>
+        {/* <Helmet title={`Breweries in "${state}" | ${config.siteTitle}`} /> */}
+        <article>
+
+          <Grid fluid>
+
+            <Row>
+
+              <Col xs={12} md={8}>
+                {
+                  breweries.map(brewery => (
+
+                    <BreweryListing key={brewery.node.id} brewery={brewery.node.context} />
+
+                  ))
+                }
+              </Col>
+
+              <Col style={{ background: 'yellow' }} xs={12} md={4} >
+                <aside style={{ display: 'flex', flexDirection: 'column' }}>
+                  {Array.from(citySet).map((city) => {
+                    return <Link to={`/${dashify(city)}/`}>{`${city} breweries`}</Link>;
+                  })}
+                </aside>
+              </Col>
+
+            </Row>
+
+          </Grid>
+        </article>
+
       </Layout>
     );
   }
 }
 
-export default Index;
+export const pageQuery = graphql`
+  query Index($state: String) {
+    allSitePage(filter: {context: {state: {eq: $state}}}) {
 
+      edges {
+        node {
+          id
+          context {
+            state
+            name
+            slug
+            city
+            latitude
+            longitude
+            street
+            website_url
+            postal_code
+            phone
+            country
+            brewery_type
+            metadata{
+              description
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+
+
+
+// export const pageQuery = graphql`
+//   query TagPage($tag: String) {
+//     allMarkdownRemark(
+//       limit: 1000
+//       sort: { fields: [fields___date], order: DESC }
+//       filter: { frontmatter: { tags: { in: [$tag] } } }
+//     ) {
+//       totalCount
+//       edges {
+//         node {
+//           fields {
+//             slug
+//             date
+//           }
+//           excerpt
+//           timeToRead
+//           frontmatter {
+//             title
+//             tags
+//             cover
+//             date
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
